7577f0acfdad9b3671dd745516a4b9a8e5ea56d3
bhav1509 <bhavnamagi@gmail.com>
2021-07-07 13:35:09 +0530

    11

--- 10_stones_on_the_table.cpp
+++ 10_stones_on_the_table.cpp
@@ -10,7 +10,18 @@ int main()
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     #endif
-    
-    
+    int n,count=0;
+    string s;
+    cin>>n>>s;
+    for(int i=0;i<n;i++)
+    {
+        //cout<<"S[i]:"<<s[i]<<endl;
+        //cout<<"S[i+1]:"<<s[i+1]<<endl;
+        if(s[i]==s[i+1])
+        {
+            count++;
+        }
+    }
+    cout<<count;
     return 0;
 }
